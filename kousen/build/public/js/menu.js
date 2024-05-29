46a6d30988ccd2306da97cc1cbc99fe71be6b397
sota-toshizumi <s-toshizumi@office-navi.co.jp>
2024-05-29 12:44:23 +0900

    皿は1皿しか選択できない

--- kousen/build/public/js/menu.js
+++ kousen/build/public/js/menu.js
@@ -4,6 +4,8 @@ function openModal(sushiType) {
     document.getElementById('modalTitle').innerHTML = sushiType; // モーダルのタイトルを設定
     document.getElementById('modalImage').src = `/public/src/${sushiType.toLowerCase()}.png`; // 対応する寿司の画像を設定
     resetQuantityButtons(); // 皿数選択ボタンの選択状態をリセット
+    defaultQuantity(); // 最初は1皿を選択しとく
+
 }
 
 // 皿数を選択するための関数
@@ -19,6 +21,11 @@ function resetQuantityButtons() {
     });
 }
 
+function defaultQuantity() {
+    resetQuantityButtons(); // すべての選択状態をリセット
+    document.querySelector('.quantity-buttons button[data-quantity="1"]').classList.add('selected'); // 1皿のボタンに選択状態を適用
+}
+
 // モーダルウィンドウを閉じるためのイベントハンドラ
 document.querySelector('.close').onclick = function() {
     document.getElementById('orderModal').style.display = 'none'; // モーダルウィンドウを非表示に
@@ -27,6 +34,6 @@ document.querySelector('.close').onclick = function() {
 // 最終的な注文を処理するためのイベントハンドラ
 document.querySelector('.final-order-button').onclick = function() {
     var selectedButton = document.querySelector('.quantity-buttons button.selected'); // 選択された皿数のボタンを取得
-    var quantity = selectedButton ? selectedButton.getAttribute('data-quantity') : 0; // 選択された皿数を取得、選択がなければ0を設定
+    var quantity = selectedButton ? selectedButton.getAttribute('data-quantity') : 1; // 選択された皿数を取得、選択がなければ0を設定
     window.location.href = `/order?item=${document.getElementById('modalTitle').textContent}&quantity=${quantity}`; // 注文ページへリダイレクトし、アイテム名と数量をクエリパラメータとして渡す
 }
