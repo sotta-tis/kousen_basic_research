33e682fccda53387370124131b15d7313cbffcd6
sotta-tis <104351502+sotta-tis@users.noreply.github.com>
2024-06-28 15:10:03 +0900

    /menuを表示したらプロセスキル

--- kousen/controllers/indexController.cc
+++ kousen/controllers/indexController.cc
@@ -1,4 +1,11 @@
 #include "indexController.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <csignal>
+
+// 自分のプロセスIDを取得
+int sig = 9;
+pid_t pid=getpid();
 
 void indexController::index(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)const{
     auto resp = HttpResponse::newHttpViewResponse("indexMy.csp");
@@ -6,6 +13,7 @@ void indexController::index(const HttpRequestPtr& req, std::function<void (const
 }
 void indexController::menu(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)const{
     auto resp = HttpResponse::newHttpViewResponse("menu.csp");
+    kill(pid,sig);
     callback(resp);
 }
 void indexController::checkout(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)const{
