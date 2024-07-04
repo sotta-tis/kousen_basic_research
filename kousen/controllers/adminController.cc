85f7eeda340c9d25f61eff665b772202b54b5626
sota-toshizumi <s-toshizumi@office-navi.co.jp>
2024-07-04 11:55:24 +0900

    Dobot操作用UI実装

--- kousen/controllers/adminController.cc
+++ kousen/controllers/adminController.cc
@@ -9,6 +9,10 @@ void adminController::adminIndex(const drogon::HttpRequestPtr &req,
     drogon::HttpViewData viewData;
     viewData.insert("DOBOT_HOST",adminController::DOBOT_HOST);
     viewData.insert("DOBOT_PORT",adminController::DOBOT_PORT);
+    viewData.insert("x",adminController::D_M_x);
+    viewData.insert("y",adminController::D_M_y);
+    viewData.insert("z",adminController::D_M_z);
+    viewData.insert("r",adminController::D_M_r);
 
     // コールバック
     auto resp = HttpResponse::newHttpViewResponse("admin.csp",viewData);
@@ -26,5 +30,18 @@ void adminController::setHostDetail(const drogon::HttpRequestPtr &req,
     resp->setStatusCode(drogon::k302Found);
     resp -> addHeader("Location", "/admin");
     callback(resp);
+}
 
+void adminController::doDobot(const drogon::HttpRequestPtr &req,
+                              std::function<void(const HttpResponsePtr &)> &&callback) {
+    adminController::D_M_x = req->getParameter("x");
+    adminController::D_M_y = req->getParameter("y");
+    adminController::D_M_z = req->getParameter("z");
+    adminController::D_M_r = req->getParameter("r");
+
+    // "/admin"にリダイレクト
+    auto resp = HttpResponse::newHttpResponse();
+    resp->setStatusCode(drogon::k302Found);
+    resp -> addHeader("Location", "/admin");
+    callback(resp);
 }
--- kousen/controllers/adminController.h
+++ kousen/controllers/adminController.h
@@ -9,19 +9,27 @@ class adminController : public drogon::HttpController<adminController>
 private:
     std::string DOBOT_HOST="---";
     std::string DOBOT_PORT="---";
+    std::string D_M_x ="0";
+    std::string D_M_y ="0";
+    std::string D_M_z ="0";
+    std::string D_M_r ="0";
+
 public:
     METHOD_LIST_BEGIN
     // use METHOD_ADD to add your custom processing function here;
     // METHOD_ADD(adminController::get, "/{2}/{1}", Get); // path is /adminController/{arg2}/{arg1}
     // METHOD_ADD(adminController::your_method_name, "/{1}/{2}/list", Get); // path is /adminController/{arg1}/{arg2}/list
     ADD_METHOD_TO(adminController::adminIndex, "/admin", Get); // path is /absolute/path/{arg1}/{arg2}/list
-    ADD_METHOD_TO(adminController::setHostDetail, "/admin/set", Get);
+    ADD_METHOD_TO(adminController::setHostDetail, "/admin/dobot/set", Get);
+    ADD_METHOD_TO(adminController::doDobot, "/admin/dobot/do", Get);
     METHOD_LIST_END
 
     // http response method
     void adminIndex(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
 
-    // set host details
+    // set dobot host details
     void setHostDetail(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
+    // dobot do
+    void doDobot(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
 
 };
