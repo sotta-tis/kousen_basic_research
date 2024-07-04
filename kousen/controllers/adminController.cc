780d8b812641a0b9b725bf272e58c3816474bcea
sota-toshizumi <s-toshizumi@office-navi.co.jp>
2024-07-04 11:25:30 +0900

    Dobot用の設定操作の追加

--- kousen/controllers/adminController.cc
+++ kousen/controllers/adminController.cc
@@ -1,17 +1,30 @@
 #include "adminController.h"
-#include "adminController.h"
 
 // Add definition of your processing function here
 
 void adminController::adminIndex(const drogon::HttpRequestPtr &req,
                                  std::function<void(const HttpResponsePtr &)>
                                  &&callback) {
-    auto resp = HttpResponse::newHttpViewResponse("admin.csp");
+    // viewData梱包
+    drogon::HttpViewData viewData;
+    viewData.insert("DOBOT_HOST",adminController::DOBOT_HOST);
+    viewData.insert("DOBOT_PORT",adminController::DOBOT_PORT);
+
+    // コールバック
+    auto resp = HttpResponse::newHttpViewResponse("admin.csp",viewData);
     callback(resp);
 }
 
 void adminController::setHostDetail(const drogon::HttpRequestPtr &req,
-                                    std::function<void(const HttpResponsePtr &)>
-                                    &&callback) {
+                                    std::function<void(const HttpResponsePtr &)> &&callback) {
+    // Dobotのipアドレスとportを更新
+    adminController::DOBOT_HOST = req->getParameter("ipAddress");
+    adminController::DOBOT_PORT = req->getParameter("port");
+
+    // "/admin"にリダイレクト
+    auto resp = HttpResponse::newHttpResponse();
+    resp->setStatusCode(drogon::k302Found);
+    resp -> addHeader("Location", "/admin");
+    callback(resp);
 
 }
--- kousen/controllers/adminController.h
+++ kousen/controllers/adminController.h
@@ -6,13 +6,16 @@ using namespace drogon;
 
 class adminController : public drogon::HttpController<adminController>
 {
-  public:
+private:
+    std::string DOBOT_HOST="---";
+    std::string DOBOT_PORT="---";
+public:
     METHOD_LIST_BEGIN
     // use METHOD_ADD to add your custom processing function here;
     // METHOD_ADD(adminController::get, "/{2}/{1}", Get); // path is /adminController/{arg2}/{arg1}
     // METHOD_ADD(adminController::your_method_name, "/{1}/{2}/list", Get); // path is /adminController/{arg1}/{arg2}/list
     ADD_METHOD_TO(adminController::adminIndex, "/admin", Get); // path is /absolute/path/{arg1}/{arg2}/list
-    ADD_METHOD_TO(adminController::setHostDetail, "/admin/set/{}{}", Get);
+    ADD_METHOD_TO(adminController::setHostDetail, "/admin/set", Get);
     METHOD_LIST_END
 
     // http response method
