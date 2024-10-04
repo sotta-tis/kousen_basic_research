a334ac793e1518ecf9cd4d8efff406e03646fc6b
sota-toshizumi <s-toshizumi@office-navi.co.jp>
2024-10-04 13:21:20 +0900

    :fire: login処理とviewのルーティング削除

--- kousen/controllers/adminController.cc
+++ kousen/controllers/adminController.cc
@@ -1,56 +1,8 @@
 #include "adminController.h"
 #include "socketCommunication.h"
 #include "killProcess.h"
-#include "loginUtility.h"
 #include "commonData.h"
 #include "servoClient.h"
-
-// Add definition of your processing function here
-// admin画面
-void adminController::admin(const HttpRequestPtr &req,
-                            std::function<void(const HttpResponsePtr &)> &&callback) const
-{
-    callback(adminController::loginFilter(req,adminController::adminIndex()));
-}
-
-drogon::HttpResponsePtr adminController::loginFilter(const HttpRequestPtr &req,drogon::HttpResponsePtr requiredRes)const{
-    bool loginState = false;
-    // login失敗時のビューデータ作成
-    auto viewData = drogon::HttpViewData();
-    viewData.insert("message", std::string(""));
-    drogon::HttpResponsePtr response =drogon::HttpResponse::newHttpViewResponse("login.csp", viewData);
-
-    // セッションからログイン済みか判断
-    drogon::SessionPtr sessionHolder = req->getSession();
-    if(sessionHolder->find("loginState")) loginState = sessionHolder->getOptional<bool>("loginState").value_or(false);
-
-    // ログイン済みであればそのままadmin画面を表示
-    if (loginState) response = requiredRes;
-
-    return response;
-}
-
-// 管理画面のレスポンスを作成
-drogon::HttpResponsePtr adminController::adminIndex()const {
-    // viewData梱包
-    drogon::HttpViewData viewData;
-    viewData.insert("PID",killPro::pid);
-    viewData.insert("DOBOT_HOST",adminController::DOBOT_HOST);
-    viewData.insert("DOBOT_PORT",adminController::DOBOT_PORT);
-    viewData.insert("x",adminController::D_M_x);
-    viewData.insert("y",adminController::D_M_y);
-    viewData.insert("z",adminController::D_M_z);
-    viewData.insert("r",adminController::D_M_r);
-
-    viewData.insert("SERVO_HOST",adminController::SERVO_HOST);
-    viewData.insert("SERVO_PORT",adminController::SERVO_PORT);
-    viewData.insert("STANDBY",adminController::standby);
-    viewData.insert("CLOSE",adminController::close);
-    viewData.insert("OPEN",adminController::open);
-
-    // コールバックレスポンス作成
-    return HttpResponse::newHttpViewResponse("admin.csp",viewData);
-}
 
 void adminController::shutDown(const drogon::HttpRequestPtr &req,
                                std::function<void(const HttpResponsePtr &)> &&callback) {
@@ -111,58 +63,6 @@ void adminController::doDobot(const drogon::HttpRequestPtr &req,
     callback(response);
 }
 
-// 新規ユーザー登録画面
-void adminController::newuser(const HttpRequestPtr &req,
-                              std::function<void(const HttpResponsePtr &)> &&callback) const
-{
-    auto response = drogon::HttpResponse::newHttpViewResponse("userAdd.csp");
-    callback(response);
-}
-
-// ユーザー情報の保存
-void adminController::submit(const HttpRequestPtr &req,
-                             std::function<void(const HttpResponsePtr &)> &&callback) const
-{
-    std::string id = req->getParameter("loginID");
-    std::string pass = req->getParameter("passWord");
-    std::string validation = req->getParameter("validation");
-    drogon::HttpResponsePtr response = drogon::HttpResponse::newHttpViewResponse("userAdd.csp");
-
-    if(pass == validation)
-    {
-        std::array<u_char, SHA512_DIGEST_LENGTH> passHash = {0};
-
-        // パスワードの暗号化ハッシュを作成します。
-        std::string digest =
-                createDigestArray<char>(
-                        pass.c_str(),
-                        static_cast<size_t>( pass.length() ),
-                        passHash
-                );
-        drogon::orm::DbClientPtr dbp =
-                drogon::app().getDbClient();
-
-        // ファイルにせよ、DataBaseにせよ、保存するときは平文ではなく暗号化ハッシュを保存します。
-        // こうすることで、保存先が露呈して盗み見られた際にも、
-        // パスワードそのものが流出することを防ぐことができます。
-        auto result = dbp->execSqlSync(
-                "INSERT INTO passwords VALUES ($1,$2);",
-                id,
-                digest
-        );
-
-        if( result.affectedRows() != 0)
-        {
-            auto viewData = drogon::HttpViewData();
-            viewData.insert("message", std::string(""));
-            response =
-                    drogon::HttpResponse::newHttpViewResponse("login.csp", viewData);
-        }
-    }
-    callback( response );
-}
-
-
 std::string adminController::getStoredDigest(std::string userID) const
 {
     std::string resultString;
@@ -180,48 +80,6 @@ std::string adminController::getStoredDigest(std::string userID) const
     return resultString;
 }
 
-// login処理
-void adminController::login(const HttpRequestPtr &req,
-                            std::function<void(const HttpResponsePtr &)> &&callback) const
-{
-    auto viewData = drogon::HttpViewData();
-
-    // エラービュー作成
-    viewData.insert("message", std::string("ログイン情報が不正です。"));
-    drogon::HttpResponsePtr response = HttpResponse::newHttpViewResponse("login.csp", viewData);
-
-    // ビューから情報取得
-    std::string preserve = req->getParameter("preserve");
-    std::string id = req->getParameter("loginID");
-    std::string pass = req->getParameter("passWord");
-
-    // hash用
-    std::array<u_char, 64UL> inputHash;
-    std::array<u_char, 64UL> storedHash;
-
-    std::string passDigest =createDigestArray<char>( pass.c_str(), static_cast<size_t>( pass.length() ), inputHash );
-
-    std::string storedDigest =getStoredDigest(id);
-
-    // login情報に問題がなければセッションの登録をして管理者画面に移動
-    if( passDigest ==  storedDigest )
-    {
-        if (preserve != "")
-        {
-            req->session()->insert("loginState", true);
-        }
-        else
-        {
-            req->session()->erase("loginState");
-        }
-        auto resp = HttpResponse::newHttpResponse();
-        resp->setStatusCode(drogon::k302Found);
-        resp -> addHeader("Location", "/admin");
-        response = resp;
-    }
-
-    callback(response);
-}
 
 void adminController::setGlipperHost(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback){
     // "/admin"にリダイレクト
--- kousen/controllers/adminController.h
+++ kousen/controllers/adminController.h
@@ -29,24 +29,16 @@ public:
     // use METHOD_ADD to add your custom processing function here;
     // METHOD_ADD(adminController::get, "/{2}/{1}", Get); // path is /adminController/{arg2}/{arg1}
     // METHOD_ADD(adminController::your_method_name, "/{1}/{2}/list", Get); // path is /adminController/{arg1}/{arg2}/list
-    ADD_METHOD_TO(adminController::login, "/login", Post);
-    ADD_METHOD_TO(adminController::admin, "/admin", Get); // path is /absolute/path/{arg1}/{arg2}/list
     ADD_METHOD_TO(adminController::setHostDetail, "/admin/dobot/set", Get);
     ADD_METHOD_TO(adminController::doDobot, "/admin/dobot/do", Get);
     ADD_METHOD_TO(adminController::setGlipperHost, "/admin/glipper/set", Get);
     ADD_METHOD_TO(adminController::setGlipperInitial, "/admin/glipper/initial", Get);
     ADD_METHOD_TO(adminController::setGlipperDo, "/admin/glipper/do/{action}", Get);
     ADD_METHOD_TO(adminController::shutDown, "/admin/kill", Get);
-    ADD_METHOD_TO(adminController::newuser, "/newuser", Get);
-    ADD_METHOD_TO(adminController::submit, "/submit", Post);
     METHOD_LIST_END
 
     // システムシャットダウン
     void shutDown(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
-
-    // http response method
-    drogon::HttpResponsePtr adminIndex()const;
-    void admin(const HttpRequestPtr &req,std::function<void(const HttpResponsePtr &)> &&callback) const;
 
     // set dobot host details
     void setHostDetail(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
@@ -58,13 +50,6 @@ public:
     void setGlipperInitial(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
     void setGlipperDo(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback,std::string action);
 
-    void newuser(const HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> &&callback) const;
-
-    void submit(const HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> &&callback) const;
-
-    void login(const HttpRequestPtr &req,std::function<void(const HttpResponsePtr &)> &&callback) const;
-
     std::string getStoredDigest(std::string userID) const;
 
-    drogon::HttpResponsePtr loginFilter(const HttpRequestPtr &req,drogon::HttpResponsePtr requiredRes)const;
 };
