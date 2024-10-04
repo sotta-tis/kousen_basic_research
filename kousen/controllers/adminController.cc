09fcdf7929be853fa8e91ccb8b0a28d2e3f1be1e
sota-toshizumi <s-toshizumi@office-navi.co.jp>
2024-10-04 14:40:26 +0900

    :hammer: 全てのエンドポイントをapiサーバーように変更

--- kousen/controllers/adminController.cc
+++ kousen/controllers/adminController.cc
@@ -3,87 +3,124 @@
 #include "killProcess.h"
 #include "commonData.h"
 #include "servoClient.h"
+#include "json.hpp"
 
 void adminController::shutDown(const drogon::HttpRequestPtr &req,
                                std::function<void(const HttpResponsePtr &)> &&callback) {
-    // "/admin/kill"にリダイレクト
-    auto resp = HttpResponse::newHttpResponse();
-    resp->setStatusCode(drogon::k302Found);
-    resp -> addHeader("Location", "/admin");
-
-    sockC::quit(adminController::DOBOT_HOST,adminController::DOBOT_PORT);
+    drogon::HttpStatusCode statusCode=drogon::k200OK;
+    Json::Value jsonResponse;
+    try {
+        sockC::quit(adminController::DOBOT_HOST,adminController::DOBOT_PORT);
+    } catch (const std::exception& e) {
+        statusCode = drogon::k500InternalServerError;
+    }
+    auto resp = HttpResponse::newHttpJsonResponse(jsonResponse);
+    resp->setStatusCode(statusCode);
+    callback(resp);
+    // システムシャットダウン
     killPro::killProcess();
 }
 
 void adminController::setHostDetail(const drogon::HttpRequestPtr &req,
                                     std::function<void(const HttpResponsePtr &)> &&callback) {
-    // "/admin"にリダイレクト
-    auto resp = HttpResponse::newHttpResponse();
-    resp->setStatusCode(drogon::k302Found);
-    resp -> addHeader("Location", "/admin");
+    drogon::HttpStatusCode statusCode=drogon::k200OK;
+    Json::Value jsonResponse;
+    try {
+        // Dobotのipアドレスとportを更新
+        adminController::DOBOT_HOST = req->getParameter("ipAddress");
+        adminController::DOBOT_PORT = std::stoi(req->getParameter("port"));
 
-    // Dobotのipアドレスとportを更新
-    adminController::DOBOT_HOST = req->getParameter("ipAddress");
-    adminController::DOBOT_PORT = std::stoi(req->getParameter("port"));
+        // 通信
+        sockC::setting(adminController::DOBOT_HOST,adminController::DOBOT_PORT);
 
-    // 通信
-    sockC::setting(adminController::DOBOT_HOST,adminController::DOBOT_PORT);
+    } catch (const std::exception& e) {
+        statusCode = drogon::k500InternalServerError;
+    }
+    auto resp = HttpResponse::newHttpJsonResponse(jsonResponse);
+    resp->setStatusCode(statusCode);
+    callback(resp);
 }
 
 void adminController::doDobot(const drogon::HttpRequestPtr &req,
                               std::function<void(const HttpResponsePtr &)> &&callback) {
-    // "/admin"にリダイレクト
-    auto resp = HttpResponse::newHttpResponse();
-    resp->setStatusCode(drogon::k302Found);
-    resp -> addHeader("Location", "/admin");
+    drogon::HttpStatusCode statusCode=drogon::k200OK;
+    Json::Value jsonResponse;
+    try{
+        adminController::D_M_x = std::stoi(req->getParameter("x"));
+        adminController::D_M_y = std::stoi(req->getParameter("y"));
+        adminController::D_M_z = std::stoi(req->getParameter("z"));
+        adminController::D_M_r = std::stoi(req->getParameter("r"));
 
-    adminController::D_M_x = std::stoi(req->getParameter("x"));
-    adminController::D_M_y = std::stoi(req->getParameter("y"));
-    adminController::D_M_z = std::stoi(req->getParameter("z"));
-    adminController::D_M_r = std::stoi(req->getParameter("r"));
-
-    // DOBOTのアームを任意座標に移動
-    sockC::moveArmParam(adminController::D_M_x,adminController::D_M_y,adminController::D_M_z,adminController::D_M_r,adminController::DOBOT_HOST,adminController::DOBOT_PORT);
+        // DOBOTのアームを任意座標に移動
+        sockC::moveArmParam(adminController::D_M_x,adminController::D_M_y,adminController::D_M_z,adminController::D_M_r,adminController::DOBOT_HOST,adminController::DOBOT_PORT);
+    }catch (const std::exception& e) {
+        statusCode= drogon::k500InternalServerError;
+    }
+    auto resp = HttpResponse::newHttpJsonResponse(jsonResponse);
+    resp->setStatusCode(statusCode);
+    callback(resp);
 }
 
 void adminController::setGlipperHost(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback){
-    // "/admin"にリダイレクト
-    auto resp = HttpResponse::newHttpResponse();
-    resp->setStatusCode(drogon::k302Found);
-    resp -> addHeader("Location", "/admin");
+    drogon::HttpStatusCode statusCode=drogon::k200OK;
+    Json::Value jsonResponse;
 
-    adminController::SERVO_HOST = req->getParameter("ipAddress");
-    adminController::SERVO_PORT = std::stoi(req->getParameter("port"));
+    try{
+        adminController::SERVO_HOST = req->getParameter("ipAddress");
+        adminController::SERVO_PORT = std::stoi(req->getParameter("port"));
 
-    servoClient = std::make_unique<ServoClient>(req->getParameter("ipAddress"),std::stoi(req->getParameter("port")));
+        servoClient = std::make_unique<ServoClient>(req->getParameter("ipAddress"),std::stoi(req->getParameter("port")));
 
-    if(servoClient->connectToServer()){
-        std::cout<<"servo connection!!"<<std::endl;
+        if(servoClient->connectToServer()){
+            std::cout<<"servo connection!!"<<std::endl;
+        }
+
+    }catch (const std::exception& e){
+        statusCode= k500InternalServerError;
     }
+
+    auto resp = HttpResponse::newHttpJsonResponse(jsonResponse);
+    resp->setStatusCode(statusCode);
+    callback(resp);
 };
 
 void adminController::setGlipperInitial(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback){
-    // "/admin"にリダイレクト
-    auto resp = HttpResponse::newHttpResponse();
-    resp->setStatusCode(drogon::k302Found);
-    resp -> addHeader("Location", "/admin");
+    drogon::HttpStatusCode statusCode=drogon::k200OK;
+    Json::Value jsonResponse;
 
-    adminController::standby = std::stoi(req->getParameter("standby"));
-    adminController::close = std::stoi(req->getParameter("close"));
-    adminController::open = std::stoi(req->getParameter("open"));
+    try {
+        adminController::standby = std::stoi(req->getParameter("standby"));
+        adminController::close = std::stoi(req->getParameter("close"));
+        adminController::open = std::stoi(req->getParameter("open"));
+    }catch(const std::exception& e){
+        statusCode=drogon::k500InternalServerError;
+    }
+
+    // JSONレスポンスを作成して返す
+    auto resp = HttpResponse::newHttpJsonResponse(jsonResponse);
+    resp->setStatusCode(statusCode);
+    callback(resp);
 };
 
 void adminController::setGlipperDo(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback,std::string action){
-    // "/admin"にリダイレクト
-    auto resp = HttpResponse::newHttpResponse();
-    resp->setStatusCode(drogon::k302Found);
-    resp -> addHeader("Location", "/admin");
-
-    if(action=="standby"){
-        servoClient->sendAngle(adminController::standby);
-    }if(action=="close"){
-        servoClient->sendAngle(adminController::close);
-    }if(action=="open"){
-        servoClient->sendAngle(adminController::open);
+    drogon::HttpStatusCode statusCode=drogon::k200OK;
+    Json::Value jsonResponse;
+    try {
+        if (action == "standby") {
+            servoClient->sendAngle(adminController::standby);
+        }
+        if (action == "close") {
+            servoClient->sendAngle(adminController::close);
+        }
+        if (action == "open") {
+            servoClient->sendAngle(adminController::open);
+        }
+    }catch (const std::exception& e){
+        statusCode = drogon::k500InternalServerError;
     }
+
+    // JSONレスポンスを作成して返す
+    auto resp = HttpResponse::newHttpJsonResponse(jsonResponse);
+    resp->setStatusCode(statusCode);
+    callback(resp);
 };
