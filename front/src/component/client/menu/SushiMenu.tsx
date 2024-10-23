6d81d0b7f4d28f0f262fa4191812ee5a5c769a6d
sota-toshizumi <s-toshizumi@office-navi.co.jp>
2024-10-23 13:59:55 +0900

    :hammer: 在庫状況に合わせてメニューを変更

--- front/src/component/client/menu/SushiMenu.tsx
+++ front/src/component/client/menu/SushiMenu.tsx
@@ -24,9 +24,15 @@ interface ItemProps {
   imageSrc: string;
   price: string;
   name: string;
+  stock: number;
 }
 
-const SushiItem: React.FC<ItemProps> = ({ imageSrc, price, name }) => {
+interface MenuProps {
+  count: number;
+  menu: { [key: string]: number };
+}
+
+const SushiItem: React.FC<ItemProps> = ({ imageSrc, price, name, stock }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [quantity, setQuantity] = useState(1);
 
@@ -34,92 +40,115 @@ const SushiItem: React.FC<ItemProps> = ({ imageSrc, price, name }) => {
 
   return (
     <>
-      <Box
-        onClick={onOpen}
-        cursor="pointer"
-        textAlign="center"
-        w="200px"
-        justifyItems={"center"}
-        borderWidth="1px"
-        borderRadius="md"
-        borderColor="gray.300"
-        _hover={{ borderColor: "blue.400", boxShadow: "md" }}
-      >
-        <Image
-          src={imageSrc}
-          alt={name}
-          boxSize="150px"
-          objectFit="scale-down"
-        />
-        <Text mt={2}>{name}</Text>
-        <Text fontWeight="bold">{price}円</Text>
-      </Box>
+      {stock ? (
+        <>
+          <Box
+            onClick={onOpen}
+            cursor="pointer"
+            textAlign="center"
+            w="200px"
+            justifyItems={"center"}
+            borderWidth="1px"
+            borderRadius="md"
+            borderColor="gray.300"
+            _hover={{ borderColor: "blue.400", boxShadow: "md" }}
+          >
+            <Image
+              src={imageSrc}
+              alt={name}
+              boxSize="150px"
+              objectFit="scale-down"
+            />
+            <Text mt={2}>{name}</Text>
+            <Text fontWeight="bold">{price}円</Text>
+          </Box>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{name}</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <HStack justifyContent={"center"}>
-              <Image
-                src={imageSrc}
-                alt={name}
-                boxSize="150px"
-                objectFit="scale-down"
-              />
-            </HStack>
-          </ModalBody>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>{name}</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <HStack justifyContent={"center"}>
+                  <Image
+                    src={imageSrc}
+                    alt={name}
+                    boxSize="150px"
+                    objectFit="scale-down"
+                  />
+                </HStack>
+              </ModalBody>
 
-          <ModalFooter>
-            <HStack
-              spacing={3}
-              alignItems="center"
-              justifyContent="flex-start"
-              w="full"
-            >
-              <Text>枚数:</Text>
-              <NumberInput
-                aria-label="ds"
-                min={1}
-                value={quantity}
-                onChange={handleQuantityChange}
-              >
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-              <Button colorScheme="blue" onClick={onClose}>
-                注文確定
-              </Button>
-            </HStack>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+              <ModalFooter>
+                <HStack
+                  spacing={3}
+                  alignItems="center"
+                  justifyContent="flex-start"
+                  w="full"
+                >
+                  <Text>枚数:</Text>
+                  <NumberInput
+                    aria-label="ds"
+                    min={1}
+                    max={stock}
+                    value={quantity}
+                    onChange={handleQuantityChange}
+                  >
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                  <Button colorScheme="blue" onClick={onClose}>
+                    注文確定
+                  </Button>
+                </HStack>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
 
-const SushiMenu: React.FC = () => {
-  const items = [
-    { imageSrc: "/salmon.png", price: "100", name: "えび", id: 6 },
-    { imageSrc: "/ikura.png", price: "100", name: "いくら", id: 1 },
-    { imageSrc: "/maguro.png", price: "100", name: "マグロ", id: 2 },
-    { imageSrc: "/ika.png", price: "100", name: "いか", id: 3 },
-    { imageSrc: "/ika.png", price: "100", name: "うに", id: 4 },
-    { imageSrc: "/ika.png", price: "100", name: "たまご", id: 5 },
-  ];
+const SushiMenu: React.FC<MenuProps> = ({ count, menu }) => {
+  const items = {
+    6: {
+      imageSrc: "/salmon.png",
+      price: "100",
+      name: "えび",
+      stock: menu["6"],
+    },
+    1: {
+      imageSrc: "/ikura.png",
+      price: "100",
+      name: "いくら",
+      stock: menu["1"],
+    },
+    2: {
+      imageSrc: "/maguro.png",
+      price: "100",
+      name: "マグロ",
+      stock: menu["2"],
+    },
+    3: { imageSrc: "/ika.png", price: "100", name: "いか", stock: menu["3"] },
+    4: { imageSrc: "/ika.png", price: "100", name: "うに", stock: menu["4"] },
+    5: { imageSrc: "/ika.png", price: "100", name: "たまご", stock: menu["5"] },
+  };
 
   return (
     <HStack spacing={8} align="flex-start" wrap={"wrap"} justify={"center"}>
-      {items.map((item, index) => (
+      {Object.values(items).map((item, index) => (
         <SushiItem
           key={index}
           imageSrc={item.imageSrc}
           price={item.price}
           name={item.name}
+          stock={item.stock}
         />
       ))}
     </HStack>
--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,14 +1,38 @@
 // pages/index.tsx
 import { Box, Button, Text } from "@chakra-ui/react";
 import Layout from "../component/layouts/client/layout";
-import OrderItems from "../component/client/menu/SushiMenu";
+import SushiMenu from "../component/client/menu/SushiMenu";
 
-const Home = (): JSX.Element => {
+const Home = ({ serverData }): JSX.Element => {
   return (
     <Layout>
-      <OrderItems />
+      <SushiMenu count={serverData.sushi.count} menu={serverData.sushi.menu} />
     </Layout>
   );
 };
 
+export const getServerSideProps = async () => {
+  try {
+    // クエリパラメータでリクエストしたいURLを指定
+    const urlToFetch = `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/`;
+    const response = await fetch(urlToFetch);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return {
+      props: {
+        serverData: data, // 取得したデータをpropsに渡す
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        error: error.message,
+      },
+    };
+  }
+};
 export default Home;
